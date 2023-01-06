802bbd355962ec55920bcb500a89e6ebd8e37ae0
CrowdFunding8 <rashwork88@gmail.com>
2023-01-06 18:59:32 +0530

    files updated app.js n index.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom"
-import Home from "./Components/Home/Home"
-import Login from "./Components/Login/Login"
-import Contact from "./Components/Contact/Contact"
-import About from "./Components/About/About"
+import Home from "./components/Home/Home"
+import Login from "./components/Login/Login"
+import Contact from "./components/Contact/Contact"
+import About from "./components/About/About"
 
 const App = () => { // component name should start with capital letter
   return ( //to access component within an component we use component as an tag
--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import NewIdea from './components/newIdea/newIdea';
-// import Idea from './components/ideas/idea';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Register from './components/register/register';
-// import Forgot from './components/forgotPassword/forgot';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Register /> */}
-    {/* <Forgot /> */}
-    {/* <Idea/> */}
-    <NewIdea/>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
