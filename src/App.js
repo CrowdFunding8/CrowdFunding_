22f726f5d67d59c92619cac212cb3b2711ee6a97
CrowdFunding8 <rashwork88@gmail.com>
2023-01-07 13:36:51 +0530

    chore: login update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import Home from "./components/Home/Home"
 import Login from "./components/Login/Login"
 import Contact from "./components/Contact/Contact"
 import About from "./components/About/About"
-<<<<<<< HEAD
-=======
-import Forgot from "./components/forgotPassword/forgot"
-import NewIdea from "./components/newIdea/newIdea"
-import Idea from "./components/ideas/idea"
->>>>>>> 50852b4193296600a32aabfea2d6e232a1c95c9c
 import Register from "./components/register/register"
 
 const App = () => { // component name should start with capital letter
@@ -18,15 +12,7 @@ const App = () => { // component name should start with capital letter
           <Route path="/login"  element={<Login />} />
           <Route path="/contact"  element={<Contact />} />
           <Route path="/about" element={<About/>}/>
-<<<<<<< HEAD
           <Route path="/register" element={<Register/>}/>
-=======
-          <Route path="/forgotPassword" element={<Forgot/>} />
-          <Route path="/Newideas" element={<NewIdea/>} />
-          <Route path="/ideas" element={<Idea/>}/>
-          <Route path="/register" element={<Register/>} />
-
->>>>>>> 50852b4193296600a32aabfea2d6e232a1c95c9c
      </Routes>
    //<> </>fragment used to when we need to use more than one tag
   )
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,57 +1,25 @@
-import stylesheet from "../Login/Login.module.css";
-import profile from "../images/profile.jpg";
-import password from "../images/password.png";
-import email from "../images/email.jpg";
-import { useState } from "react";
+import style from "../Login/Login.module.css";
 
 function Login() {
-  const [username, setUsername] = useState("")
-  const [pass, setPassword] = useState("")
-  const [selectBox] = useState("")
-
-  //username
-  const handleUsername=(event)=>{
-    console.log(event.target.value);
-    setUsername(event.target.value)
-  }
-  //password
-  const handlePassword=(event)=>{
-    console.log(event.target.value);
-    setPassword(event.target.value)
-  }
-  //login button
-  const handleLogin= () => {
-    console.log(username,pass,selectBox)
-  }
-
-
   return(
-    <div className={stylesheet.main}>
-      <div className={stylesheet.sub_main}>
+      <div className={style.MainContainer}>
         <div>
-          <div className={stylesheet.imgs}>
-            <div className={stylesheet.container_images}>
-              <img src={profile} alt="profile" className={stylesheet.profile} />
+          <div className={style.WelcomeText}>
+            <h1>Welcome</h1><br/><br/><br/>
+            <div className={style.InputContainer}>
+              <br/><br/><br/>
+              <input type="text" placeholder="Enter Username"/>
+              <input type="text" placeholder="Enter Password"/>
+              <div>
+                <button>Login</button>
+              </div>
+              <div className={style.forgotpassword}>
+                <a href="/Forgot">Forgot Password ?</a>
+              </div>
             </div>
           </div>
-            <div>
-              <h1>Login Page</h1>
-              <div>
-                <img src={email} alt="email" className={stylesheet.email} />
-                <input type="text" placeholder="Username" className={stylesheet.name} onChange={handleUsername} value={username}/>
-              </div>
-              <div className={stylesheet.second_input}>
-                <img src={password} alt="password" className={stylesheet.email} />
-                <input type="password" placeholder="Password" className={stylesheet.name} onChange={handlePassword} value={pass}/>
-              </div>
-              <div className={stylesheet.login_button}>
-                <button>Cancel</button>&emsp;&emsp;
-                <button onClick={handleLogin}>Login</button>
-              </div>
-            </div>
-         </div>
+        </div>
       </div>
-    </div>
   )
 }
 export default Login;
